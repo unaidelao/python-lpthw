c67e1fdaed58ecf14a11a2a02c76ffdcb5a4d335
unaidelao <unai.delao@gmail.com>
2018-12-13 00:24:42 +0100

    Add ex11.py

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+# input() permite pedir el dato y alojarlo usando un prompt como argumento
+name = input("What's your name?")
+
+print(f"So, {name}, you're {age} old, {height} tall and {weight} heavy.")
