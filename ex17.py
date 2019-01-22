29d9a2b66d452534b7821d5e88ef72d9e2a1002d
unaidelao <unai.delao@gmail.com>
2019-01-22 20:19:14 +0100

    Add ex17.py

---
+++ ex17.py
@@ -0,0 +1,27 @@
+# Programa que copia el contenido de un archivo en otro.
+# Si no existe el segundo, lo crea. Si ya existiese, lo sobreescribe.
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+# we could do these two on one line, how?
+# solución en archivo ex17_2.py
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
