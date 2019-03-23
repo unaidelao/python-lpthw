0e0733add4baa42b8b97c63c7ddc9adbf77c7d30
unaidelao <unai.delao@gmail.com>
2019-03-23 01:54:02 +0100

    Add MyStuff.py

---
+++ MyStuff.py
@@ -0,0 +1,13 @@
+# ex40_2
+
+class MyStuff(object):
+
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+		
+	def apple(self):
+		print("I AM CLASSY APPLES!")
+	
+thing = MyStuff() # instancia de un objeto de tipo MyStuff
+thing.apple()
+print(thing.tangerine)
